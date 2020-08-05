f44c21551be4166884e1c78c5eaadeabdc3629e3
VAN PERSIE <duckstime@sina.com>
2020-08-05 14:39:54 +0800

    [what]解决vivo 手机数据发送时卡顿的问题

--- app/src/main/java/me/hekr/sthome/tools/SendSceneData.java
+++ app/src/main/java/me/hekr/sthome/tools/SendSceneData.java
@@ -14,6 +14,7 @@ import me.hekr.sthome.R;
 import me.hekr.sthome.autoudp.ControllerWifi;
 import me.hekr.sthome.crc.CoderUtils;
 import me.hekr.sthome.debugWindow.ViewWindow;
+import me.hekr.sthome.service.NetWorkUtils;
 import me.hekr.sthome.service.SiterwellUtil;
 
 /**
@@ -39,7 +40,7 @@ public abstract class SendSceneData {
      * @param groupCode
      */
     private void sendAction(final  String groupCode){
-        boolean status = NetWorkStatusUtil.getInstance().getNetWorkStatus();
+        boolean status = NetWorkUtils.isNetworkAvailable(context);
         if (!status){
             Toast.makeText(context, context.getString(R.string.net_error), Toast.LENGTH_SHORT).show();
             return;
